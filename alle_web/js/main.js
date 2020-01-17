2e7e0d6246f0ab69a9013ab37ff8fd0e575cf402
missmikan <okamy5268@gmail.com>
2020-01-17 17:17:46 +0800

    1/17

--- alle_web/js/main.js
+++ alle_web/js/main.js
@@ -34,7 +34,7 @@
   //Nivo slider
   //---------------------------------------------
   $('#ensign-nivoslider').nivoSlider({
-    effect: 'random',
+    effect: 'fade',
     slices: 15,
     boxCols: 12,
     boxRows: 8,
@@ -198,16 +198,16 @@
     /*---------------------
    TOP Menu Stick
   --------------------- */
-  // var s = $("#sticker");
-  // var pos = s.position();
-  // $(window).on('scroll', function() {
-  //   var windowpos = $(window).scrollTop() > 300;
-  //   if (windowpos > pos.top) {
-  //     s.addClass("stick");
-  //   } else {
-  //     s.removeClass("stick");
-  //   }
-  // });
+  var s = $("#sticker");
+  var pos = s.position();
+  $(window).on('scroll', function() {
+    var windowpos = $(window).scrollTop() > 300;
+    if (windowpos > pos.top) {
+      s.addClass("stick");
+    } else {
+      s.removeClass("stick");
+    }
+  });
 
   // When the user scrolls the page, execute myFunction
   window.onscroll = function() {myFunction()};
@@ -226,33 +226,11 @@
       sticker.classList.remove("stick");
     }
   }
-
 })(jQuery);
-
-
-$(document).ready(function(){
-//Examples of how to assign the Colorbox event to elements
-$(".iframe").colorbox({iframe:true, width:"90%", height:"80%"});
-$(".callbacks").colorbox({
-onOpen:function(){ alert('onOpen: colorbox is about to open'); },
-onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
-onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
-onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
-onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
-});
-$('.non-retina').colorbox({rel:'group5', transition:'none'})
-$('.retina').colorbox({rel:'group5', transition:'none', retinaImage:true, retinaUrl:true});
-
-//Example of preserving a JavaScript event for inline calls.
-$("#click").click(function(){
-$('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
-return false;
-});
-});
 
 $(document).ready(function(){
   $("#ProductList").click(function(){
-  $(".ProductList").slideDown();
+  $(".ProductList").slideToggle();
   });
 });
 $(document).ready(function(){
@@ -260,3 +238,4 @@ $(document).ready(function(){
   $(".ProductList").slideUp();
   });
 });
+
