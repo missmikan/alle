70fd04bebd0a69cf2fac0c961cd637d2b1abb8b4
missmikan <okamy5268@gmail.com>
2020-01-20 17:43:00 +0800

    test

--- alle_web_test/js/main.js
+++ alle_web_test/js/main.js
@@ -1,3 +1,24 @@
+   var changePage = function(page) {
+    console.log("page => ", page);
+    $("#includedContent").load(page + ".html"); 
+    $("#titleContent").text("--- " + page + " ---"); 
+  };
+
+  $(function() {
+    var tabs = document.getElementsByClassName("tab");
+    console.log("tabs => ", tabs);
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        if (current.length > 0) { 
+          current[0].className = current[0].className.replace(" active", "");
+        }
+        this.className += " active";
+        });
+      }
+      $("#includedContent").load("main_slider.html");
+  });
+
 (function($) {
   "use strict";
 
@@ -20,26 +41,7 @@
   //   main_menu.removeClass("active");
   //   $(this).addClass("active");
   // });
-    var changePage = function(page) {
-    console.log("page => ", page);
-    $("#includedContent").load(page + ".html"); 
-    $("#titleContent").text("--- " + page + " ---"); 
-  };
-
-  $(function() {
-    var tabs = document.getElementsByClassName("tab");
-    console.log("tabs => ", tabs);
-    for (var i = 0; i < tabs.length; i++) {
-      tabs[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        if (current.length > 0) { 
-          current[0].className = current[0].className.replace(" active", "");
-        }
-        this.className += " active";
-        });
-      }
-      $("#includedContent").load("main_slider.html");
-  });
+ 
 
   /*----------------------------
    wow js active
